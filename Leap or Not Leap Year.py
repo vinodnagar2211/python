b9ff4b947c79d2cf1b1eecb41e8c9f7a4f3c7d19
vinodnagar2211 <vinodnagar2211@gmail.com>
2023-08-09 17:57:43 +0530

    uplod New File

---
+++ Leap or Not Leap Year.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[61]:
+
+
+#         Leap or Not Leap Year
+year=int(input("Enter The Year"))
+
+if(year%400==0 and year%100==0):
+    print("This Year is Leap")
+elif(year%4==0 and year%100!=0):
+    print("This Year is Leap")
+else:
+    print("This Year is Not Leap")
+
+
+# In[ ]:
+
+
+
+
+
+# ### 
+
+# In[ ]:
+
+
+
+
