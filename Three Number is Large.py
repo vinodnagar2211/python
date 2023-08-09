398434a61ccd622444e693040d6c75fe19af740c
vinodnagar2211 <vinodnagar2211@gmail.com>
2023-08-09 17:53:04 +0530

    uplod File

---
+++ Three Number is Large.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[68]:
+
+
+#           Three Number is Large
+a,b,c=(input("Enter Value of a and b and c")).split()
+a=int(a)
+b=int(b)
+c=int(c)
+if(a>b and a>c):
+    print("a is Large")
+elif (b>a and b>c):
+    print("b is Large")
+elif (c>a and c>b):
+    print("c is Large")
+
+
+# In[ ]:
+
+
+
+
+
+# ### 
+
+# In[ ]:
+
+
+
+
