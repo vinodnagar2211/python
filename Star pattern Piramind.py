432e44a286d44bd20d339bb943eeb53d3566f2a7
vinodnagar2211 <vinodnagar2211@gmail.com>
2023-08-17 23:42:56 +0530

    uplod file

---
+++ Star pattern Piramind.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+#              Star pattern
+
+for i in range(1,6,1):
+    for j in range(1,i+1,1):
+        print("*",end=(""))
+    print("\n")
+
+
+# In[ ]:
+
+
+
+
