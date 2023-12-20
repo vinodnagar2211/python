38611dc590572148391e636ac8458f51cd8f5170
vinodnagar2211 <vinodnagar2211@gmail.com>
2023-12-20 09:15:30 +0530

    today

---
+++ Tringle Area  with user input.c
@@ -0,0 +1,20 @@
+// Tringle Area  with user input
+#include <stdio.h>
+int main()
+{
+	
+	int base;
+	int hight;
+	
+	printf(" Enter the base :- ");
+	scanf("%d",&base);
+	
+	printf(" Enter the base :- ");
+	scanf("%d",&hight);
+	
+	area= 1/2*(base*hight);
+	
+	printf(" Tringle Area is = %d",area);
+	
+	return 0;
+}
