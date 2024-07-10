82f2284518140911bd44c2ca10ab3ad8e835094a
vinodnagar2211 <vinodnagar2211@gmail.com>
2024-07-10 22:47:41 +0530

    today

---
+++ sql.sql
@@ -0,0 +1,15 @@
+SELECT  district  as ram FROM sakila.address;
+
+select * 
+from actor
+where actor_id=4;
+/* comment 
+comment
+comment
+*/
+
+select *
+from actor
+where actpr_id in (2,3,4,5,6);
+
+
